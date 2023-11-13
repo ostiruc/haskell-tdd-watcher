3eef0a30d648b14ba7bbdacc6dc86df9cb76650b
Curtis Onuczko <ositruc@gmail.com>
2023-11-13 07:22:18 -0700

    run the tests in the watch dir

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,21 @@ module Main where
 
 import System.Environment
 import System.Directory
+import System.Process
 
+resolvePathToWatch :: IO String
 resolvePathToWatch = do
     args <- getArgs
     path <- (makeAbsolute (head args))
     return path
 
+runTests :: String -> IO ()
+runTests dir = do 
+    (_, Just hout, _, _) <- createProcess (proc "cabal" ["test"]) { cwd = Just dir }
+    print hout
+
 main :: IO ()
 main = do 
     pathToWatch <- resolvePathToWatch
     putStrLn ("Watching: " ++ pathToWatch)
+    runTests pathToWatch
