e370ce56c345b8586b33e605eaeb961645c4a8ca
Curtis Onuczko <ositruc@gmail.com>
2023-11-14 21:27:49 -0700

    moving rest of code into helpers

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,18 @@
 module Main where
 
 import Control.Concurrent
-import System.Environment
-import System.Directory
-import System.Process
 
 import Helpers
 
-resolvePathToWatch :: IO String
-resolvePathToWatch = do
-    args <- getArgs
-    path <- (makeAbsolute (head args))
-    return path
-
-runTests :: String -> IO ()
-runTests dir = do 
-    putStrLn "\n*** TEST RUN ***\n"
-    output <- readCreateProcess (proc "cabal" ["test"]) { cwd = Just dir } ""
-    putStrLn output
-    putStrLn "\n*** TEST COMPLETE ***\n"
-
-hasChanges :: String -> IO Bool
-hasChanges filePath = do
-    isDir <- doesDirectoryExist filePath
-    if isDir then do
-        dirContents <- getDirectoryContents filePath
-        print dirContents
-        hasChanges (filePath ++ "imaginary-file.hs")
-    else if endsWith ".hs" filePath then do
-        putStrLn ("Found .hs file: " ++ filePath)
-        return True
-    else
-        return False
 
 mainLoop :: String -> IO ()
 mainLoop pathToWatch = do
     threadDelay 1000000
     hasChanges <- hasChanges pathToWatch
-    if hasChanges then runTests pathToWatch else putStrLn ("skipped")
+    if hasChanges then 
+        runTests pathToWatch 
+    else 
+        putStrLn ("skipped")
     mainLoop pathToWatch
 
 main :: IO ()
--- lib/Helpers.hs
+++ lib/Helpers.hs
@@ -1,7 +1,37 @@
 module Helpers where
 
+import System.Environment
+import System.Directory
+import System.Process
+
 endsWith :: String -> String -> Bool
 endsWith pattern s
     | s == pattern = True
     | s == [] = False
     | otherwise = endsWith pattern (tail s)
+
+resolvePathToWatch :: IO String
+resolvePathToWatch = do
+    args <- getArgs
+    path <- (makeAbsolute (head args))
+    return path
+
+runTests :: String -> IO ()
+runTests dir = do 
+    putStrLn "\n*** TEST RUN ***\n"
+    output <- readCreateProcess (proc "cabal" ["test"]) { cwd = Just dir } ""
+    putStrLn output
+    putStrLn "\n*** TEST COMPLETE ***\n"
+
+hasChanges :: String -> IO Bool
+hasChanges filePath = do
+    isDir <- doesDirectoryExist filePath
+    if isDir then do
+        dirContents <- getDirectoryContents filePath
+        print dirContents
+        hasChanges (filePath ++ "imaginary-file.hs")
+    else if endsWith ".hs" filePath then do
+        putStrLn ("Found .hs file: " ++ filePath)
+        return True
+    else
+        return False
