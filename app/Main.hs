8e5687c3b70791a9010c4dd4e6b7981a1d34548d
Curtis Onuczko <ositruc@gmail.com>
2023-11-15 21:54:49 -0700

    main loop now runs tests only when files change

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,23 @@ import Control.Concurrent
 
 import Helpers
 
-
-mainLoop :: String -> IO ()
-mainLoop pathToWatch = do
+mainLoop :: String -> [WatchedFile] -> IO ()
+mainLoop pathToWatch filesBefore = do
     threadDelay 1000000
-    hasChanges <- hasChanges [pathToWatch]
-    if hasChanges then 
+    files <- filesUnderPath pathToWatch [".", "..", "dist-newstyle", ".git"]
+    filesAfter <- watchFiles (filter (endsWith ".hs") files)
+    if filesBefore /= filesAfter then 
         runTests pathToWatch 
     else 
         putStrLn ("skipped")
-    mainLoop pathToWatch
+    mainLoop pathToWatch filesAfter
 
 main :: IO ()
 main = do
     pathToWatch <- resolvePathToWatch
     putStrLn ("Watching: " ++ pathToWatch)
     files <- filesUnderPath pathToWatch [".", "..", "dist-newstyle", ".git"]
-    --print $ files
-    let hsFiles = filter (endsWith ".hs") files
-    watchBefore <- watchFiles hsFiles
-    watchAfter <- watchFiles hsFiles
+    filesBefore <- watchFiles (filter (endsWith ".hs") files)
 
-    print (watchBefore == watchAfter)
-
-    -- runTests pathToWatch
-    -- mainLoop pathToWatch
+    runTests pathToWatch
+    mainLoop pathToWatch filesBefore
